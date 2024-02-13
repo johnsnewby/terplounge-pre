c968fc5486263dc8e9766e47876c3f6584f0b514
John Newby <jsnewby@gmail.com>
2024-02-13 14:18:24 +0100

    UI improved

--- client/js/practise.js
+++ client/js/practise.js
@@ -1,3 +1,103 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9d274566126e8c15cb4f4a877c00763f0a9830da9f4263d8f506127d9a74e28f
-size 2993
+
+export async function getSources() {
+  const response = await fetch("assets/assets.json");
+  const list = await response.json();
+  let sources = [];
+  for (const item of list) {
+    let url = "assets/" + item + "/metadata.json";
+    let response = await fetch(url);
+    let metadata = await response.json();
+    await sources.push({ directory: item, metadata: metadata });
+  }
+  return sources;
+}
+
+export const getLanguageName = (lang) => {
+  const languageNames = {
+    de: "German",
+    en: "English",
+    fr: "French",
+    es: "Spanish",
+    it: "Italian"
+  }
+
+  return languageNames[lang] || lang;
+}
+
+export const getAllLanguages = async () => {
+  const sources = await getSources();
+  const languages = [
+    ...new Set(sources.map((source) => source.metadata.native)),
+  ];
+  return languages;
+}
+
+export const getTargetLanguagesForSourceLanguage = async (sourceLanguage) => {
+  const sources = await getSources();
+  const metadatas = await sources.map((source) => source.metadata);
+  const withSource = metadatas.filter((m) => m.native === sourceLanguage);
+  let targetLanguages = [];
+  for (var source of withSource) {
+    for (var translation of source.translations) {
+      targetLanguages.push(Object.keys(translation)[0]);
+    }
+  }
+  return [...new Set(targetLanguages)];
+}
+    
+export async function populateSourceSelector() {
+  const sources = await getSources();
+  const source_selector = document.getElementById("source-selector");
+  for (var i = 0; i < sources.length; i++) {
+    const item = sources[i];
+    let option = document.createElement("option");
+    option.text = item.metadata.name  + " (" + getLanguageName(item.metadata.native) + ")";
+    option.value = item.directory;
+    source_selector.add(option);
+  }
+}
+
+export async function getCompatiblePracticeMaterials(source,target) {
+  let sources = await getSources();
+  let compatiblePracticeMaterials = [];
+  for ( var candidate of sources ) {
+    if (candidate.metadata.native !== source) {
+      continue;
+    }
+    for (var dest of candidate.metadata.translations) {
+      if (dest[target]) {
+        compatiblePracticeMaterials.push(candidate);
+        continue;
+      }
+    }
+  }
+    return compatiblePracticeMaterials;
+}
+
+export async function sourceChanged() {
+  let source_selector = document.getElementById("source-selector");
+  let source_dir = source_selector.value;
+  if (source_dir === "") {
+    return;
+  }
+  let asset_dir = "assets/" + source_dir + "/";
+  let response = await fetch(asset_dir + "metadata.json");
+  let metadata = await response.json();
+  let player = document.getElementById("player");
+  player.src = asset_dir + metadata.audio;
+  let player_figure = document.getElementById("player-figure");
+  let player_figure_caption = document.getElementById("player-caption");
+  player_figure_caption.innerHTML = metadata.name;
+  const langageSelector = document.getElementById("lang");
+  while (langageSelector.options.length) {
+    langageSelector.remove(0);
+  }
+  for (const language of metadata.translations) {
+    let option = document.createElement("option");
+    option.value = Object.keys(language)[0];
+    option.text = getLanguageName(option.value);
+    langageSelector.add(option);
+  }
+      
+}
+
